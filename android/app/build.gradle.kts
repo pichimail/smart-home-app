a96c2ec58b2421274177d9c2d0f056c2cde558d2
pichimail <pichimail24@gmail.com>
2026-04-14 05:45:15 +0530

    feat: Initialize Flutter project with Android Gradle configuration and VSCode settings
    - Added .flutter-plugins-dependencies files for plugin management.
    - Created .vscode/settings.json to configure Java build settings.
    - Implemented Android build.gradle.kts for application configuration, including compile options and default settings.
    - Established android/build.gradle.kts for project-wide repository management and custom build directory.
    - Set up android/settings.gradle.kts for plugin management and inclusion of Flutter SDK.

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("com.google.gms.google-services")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.chinna.smart_hub"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.chinna.smart_hub"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
--- android/app/src/main/kotlin/com/chinna/smart_hub/MainActivity.kt
+++ android/app/src/main/kotlin/com/chinna/smart_hub/MainActivity.kt
@@ -2,5 +2,4 @@ package com.chinna.smart_hub
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
